519d90824a58cf63d7b0c2f3d1e8fc8a2a22bfb0
Salih121 <45142152+Salih121@users.noreply.github.com>
2018-12-16 04:33:17 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("523664383965134858")
+setInterval(function() {
+channel.send(`#daily`);
+}, 30)
+})
+
+client.login("NTIzNjU4NTk2ODIzNzI4MTM3.Dvc3YA.cBib6a_YgWv3VIiu3vzfBQD1RGM");
